2265b8a8624a7fdae84e8b0effa39d9bc88ed3da
Oleksandr Buhaiov <Oleksandr_Buhaiov@epam.com>
2018-05-11 19:10:35 +0300

    initial commit

---
+++ src/main/java/com/osb/springcloudcntrct/SpringCloudCntrctApplication.java
@@ -0,0 +1,12 @@
+package com.osb.springcloudcntrct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCntrctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCntrctApplication.class, args);
+    }
+}
---
+++ src/test/java/com/osb/springcloudcntrct/SpringCloudCntrctApplicationTests.java
@@ -0,0 +1,16 @@
+package com.osb.springcloudcntrct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudCntrctApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
